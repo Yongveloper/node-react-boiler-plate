b8c52974b6ddd59efdaadaa2286e2612c22176c9
Yongveloper <aaa9015@naver.com>
2021-01-14 00:20:05 +0900

    Finish boiler-plate

--- server/models/User.js
+++ server/models/User.js
@@ -25,9 +25,7 @@ const userSchema = mongoose.Schema({
     type: Number,
     default: 0,
   },
-  image: {
-    String,
-  },
+  image: String,
   token: {
     type: String,
   },
@@ -39,6 +37,7 @@ const userSchema = mongoose.Schema({
 userSchema.pre('save', function (next) {
   var user = this;
   if (user.isModified('password')) {
+    //비밀번호를 암호화 시킨다.
     bcrypt.genSalt(saltRounds, function (err, salt) {
       if (err) return next(err);
 
@@ -54,6 +53,7 @@ userSchema.pre('save', function (next) {
 });
 
 userSchema.methods.comparePassword = function (plainPassword, cb) {
+  //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
   bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
     if (err) return cb(err);
     cb(null, isMatch);
@@ -62,9 +62,13 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
 
 userSchema.methods.generateToken = function (cb) {
   var user = this;
+  // console.log('user._id', user._id)
 
-  // generate token using jsonwebToken
+  // jsonwebtoken을 이용해서 token을 생성하기
   var token = jwt.sign(user._id.toHexString(), 'secretToken');
+  // user._id + 'secretToken' = token
+  // ->
+  // 'secretToken' -> user._id
 
   user.token = token;
   user.save(function (err, user) {
@@ -75,10 +79,11 @@ userSchema.methods.generateToken = function (cb) {
 
 userSchema.statics.findByToken = function (token, cb) {
   var user = this;
-  //user._id + "" = token;
-  //decoding token
+  // user._id + ''  = token
+  //토큰을 decode 한다.
   jwt.verify(token, 'secretToken', function (err, decoded) {
-    //using userId, check token in client and DB
+    //유저 아이디를 이용해서 유저를 찾은 다음에
+    //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
     user.findOne({ _id: decoded, token: token }, function (err, user) {
       if (err) return cb(err);
       cb(null, user);
